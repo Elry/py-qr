4749568ddf12d7e712cc974ed8a0868b13e0418f
Elry <rafaelgarcialima@outlook.com>
2020-09-26 17:39:33 -0300

    code updated to use functions and a proper message to use www

--- qrcode.py
+++ qrcode.py
@@ -1,20 +1,23 @@
 import pyqrcode
 import validators
 
-print('Enter link to transform into QR code')
-url = str(input())
-
-while validators.url(url) != True:
-  print('Invalid. E.g: Https://gogogle.com')
-  print('Enter link to transform into QR code')
-  url = str(input())
-else:
+def createQR(url = 'https://www.github.com/Elry', qrName = 'author'):
   url = pyqrcode.create(url)
-  url.svg('customQR.svg', scale = 8)
-  url.eps('customQR.eps', scale = 2)
+  url.svg(qrName+'.svg', scale = 8)
+  url.eps(qrName+'.eps', scale = 2)
   print(url.terminal(quiet_zone = 1))
 
-url = pyqrcode.create('https://github.com/Elry')
-url.svg('authorQR.svg', scale = 8)
-url.eps('authorQR.eps', scale = 2)
-print(url.terminal(quiet_zone = 1))
+def getUrl():
+  print('Enter link to transform into QR code')
+  url = str(input())
+
+  while validators.url(url) != True:
+    print('Invalid url. E.g: https://www.google.com')
+    print('Enter link to transform into QR code')
+    url = str(input())
+  else:
+    createQR(url, 'customQR')
+
+  createQR()
+
+getUrl()
