b333a3d2a4e685262105fa238bc59340e2f6b8ed
Elry <rafaelgarcialima@outlook.com>
2020-09-26 17:27:22 -0300

    Added a simple functionality to generate custom QR codes as well

--- qrcode.py
+++ qrcode.py
@@ -1,6 +1,20 @@
 import pyqrcode
+import validators
+
+print('Enter link to transform into QR code')
+url = str(input())
+
+while validators.url(url) != True:
+  print('Invalid. E.g: Https://gogogle.com')
+  print('Enter link to transform into QR code')
+  url = str(input())
+else:
+  url = pyqrcode.create(url)
+  url.svg('customQR.svg', scale = 8)
+  url.eps('customQR.eps', scale = 2)
+  print(url.terminal(quiet_zone = 1))
 
 url = pyqrcode.create('https://github.com/Elry')
-url.svg('github.svg', scale = 8)
-url.eps('github.eps', scale = 2)
+url.svg('authorQR.svg', scale = 8)
+url.eps('authorQR.eps', scale = 2)
 print(url.terminal(quiet_zone = 1))
